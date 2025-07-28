95850fa071ced2e9b479b641c40448dbafd6b324
Nitesh kumar <niteshkumaryadav813@gmail.com>
2025-07-28 23:23:01 +0530

    new changes

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,4 +1,11 @@
-export const USER_API_END_POINT="http://localhost:8000/api/v1/user";
-export const JOB_API_END_POINT="http://localhost:8000/api/v1/job";
-export const APPLICATION_API_END_POINT="http://localhost:8000/api/v1/application";
-export const COMPANY_API_END_POINT="http://localhost:8000/api/v1/company";
+// export const USER_API_END_POINT="http://localhost:8000/api/v1/user";
+// export const JOB_API_END_POINT="http://localhost:8000/api/v1/job";
+// export const APPLICATION_API_END_POINT="http://localhost:8000/api/v1/application";
+// export const COMPANY_API_END_POINT = "http://localhost:8000/api/v1/company";
+
+const BASE_API = import.meta.env.VITE_BASE_API_URL;
+
+export const USER_API_END_POINT = `${BASE_API}/user`;
+export const JOB_API_END_POINT = `${BASE_API}/job`;
+export const APPLICATION_API_END_POINT = `${BASE_API}/application`;
+export const COMPANY_API_END_POINT = `${BASE_API}/company`;
